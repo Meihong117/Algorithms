a30787e71f141dc11f84afdb592d521ce5eab7a6
Estelle <estelle.wu117@gmail.com>
2021-11-26 12:32:23 +1300

    conditional

---
+++ Basic/Operator.js
@@ -0,0 +1,25 @@
+//run the js: node filename.js
+let a=5, b=7
+
+//1. Arithmetic Operators
+let division = b / a //1.4
+let division_remainder = 13.5555 % 2 // output(integer)= 0 or 1
+
+let post_increment = a++ // will be 5 here and 6 in the next line
+let pre_increment = ++a; // 7
+let post_decrement = a--; // will be 7 here and 6 in the next line
+let pre_decrement = --a; // 5
+
+
+//2. Comparison Operators: >, <,  ==, ===, !=
+
+// 3. Logical Operators: &&, ||, !
+
+// 4. Assignment Operators: +=, -=, *=, ...
+
+// 5. Conditional Operators
+
+// 6. Ternary Operator: c = a > b? a : b;
+
+
+console.log(division_remainder)  
